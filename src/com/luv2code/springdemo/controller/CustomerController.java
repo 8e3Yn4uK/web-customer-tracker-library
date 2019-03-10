1aff25d32eade54fcad75c673a51eb30e34d241c
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-03-10 11:46:13 +0100

    dao

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,8 +1,13 @@
 package com.luv2code.springdemo.controller;
 
+import com.luv2code.springdemo.dao.ICustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by 8e3Yn4uK on 09.03.2019
@@ -12,10 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @Autowired
+    private ICustomerDAO customerDAO;
+
     @RequestMapping("/list")
     public String listCustomers(Model theModel){
 
+        List<Customer> theCustomers = customerDAO.getCustomers();
+        theModel.addAttribute("customers", theCustomers);
         return "list-customers";
-
     }
 }
---
+++ src/com/luv2code/springdemo/dao/ICustomerDAO.java
@@ -0,0 +1,15 @@
+package com.luv2code.springdemo.dao;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+/**
+ * Created by 8e3Yn4uK on 10.03.2019
+ */
+
+public interface ICustomerDAO {
+
+    public List<Customer> getCustomers();
+
+}
