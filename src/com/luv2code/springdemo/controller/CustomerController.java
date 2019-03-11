e649582826363ae321dd381c3234c20c054e2dbd
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-03-11 12:40:03 +0100

    service layer

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,10 +1,11 @@
 package com.luv2code.springdemo.controller;
 
-import com.luv2code.springdemo.dao.ICustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.ICustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,13 +18,16 @@ import java.util.List;
 @RequestMapping("/customer")
 public class CustomerController {
 
+    // injecting our customer service
     @Autowired
-    private ICustomerDAO customerDAO;
+    private ICustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel){
 
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        // get customers from the service
+        List<Customer> theCustomers = customerService.getCustomers();
+        // add the customers to the model
         theModel.addAttribute("customers", theCustomers);
         return "list-customers";
     }
---
+++ src/com/luv2code/springdemo/service/ICustomerService.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo.service;
+
+import com.luv2code.springdemo.entity.Customer;
+
+import java.util.List;
+
+/**
+ * Created by 8e3Yn4uK on 11.03.2019
+ */
+
+public interface ICustomerService {
+
+    public List<Customer> getCustomers();
+}
