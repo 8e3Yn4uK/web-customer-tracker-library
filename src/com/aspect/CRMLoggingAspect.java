b9abceb585cb5fffb1f02c563508cb92b3eb0224
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-03-19 21:56:59 +0100

    add Logging with AOP

---
+++ src/com/aspect/CRMLoggingAspect.java
@@ -0,0 +1,26 @@
+package com.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by 8e3Yn4uK on 19.03.2019
+ */
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    @Before("com.aspect.PointcutExpressions.forAppFlow()")
+    public void before(JoinPoint theJoinPoint){
+        String method = theJoinPoint.getSignature().toShortString();
+        myLogger.info("======> In @Before Calling method " + method);
+    }
+
+}
---
+++ src/com/aspect/PointcutExpressions.java
@@ -0,0 +1,24 @@
+package com.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Created by 8e3Yn4uK on 19.03.2019
+ */
+
+@Aspect
+public class PointcutExpressions {
+
+    @Pointcut("execution(* com.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.dao.*.*(..))")
+    private void forDaoPackage(){}
+
+    @Pointcut("execution(* com.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("forControllerPackage() || forDaoPackage() || forServicePackage()")
+    public void forAppFlow(){}
+}
